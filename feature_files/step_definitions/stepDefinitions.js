bb3e5de80b22e9d78018c137ef3e3571df27f7cd
sarah asghedom-ananga <sarah.asghedom-ananga@persgroep.nl>
2017-02-09 11:09:58 +0100

    Edit test

--- feature_files/step_definitions/stepDefinitions.js
+++ feature_files/step_definitions/stepDefinitions.js
@@ -2,11 +2,12 @@ module.exports = function() {
 var page = require(__dirname + '/page_objects/computerDatabase.js');
 
   this.Given(/^I go to the computers database site$/, function() {
-browser.get('http://computer-database.herokuapp.com/computers');
+browser.driver.get('http://computer-database.herokuapp.com/computers');
   });
 
   this.When(/^I create a new computer name$/, function(callback){
-   page.newComputerButton.click();
+//   page.newComputerButton.click();
+   browser.driver.find(By.id('add'))
     });
 
 };
